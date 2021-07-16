1a5a3a38e42883105d067313853163fb3e6f9919
troytroyhi <rtysu789@gmail.com>
2021-07-16 18:14:39 +0900

    0716

---
+++ condition.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main_condition(void)
+{
+  //버스를 탄다고 가정. 학생/일반인으로 구분(일반인:20세)
+  // int age = 15;
+  // if (age>=20)
+  //   printf("일반인 입니다");
+  // else
+  //   printf("학생입니다.");
+  // return 0;
+
+  // 초등학생(8-13) / 중학생(14-16) / 고등학생(17-19) 으로 나누면?
+  // int age = 8;
+  // if(age>=8 && age<=13)
+  // {
+  //   printf("초등학생입니다.\n");
+  // }
+  // else if (age>=14 && age<=16)
+  // {
+  //   printf("중학생입니다.\n");
+  // }
+  // else if (age>=17 && age<=19)
+  // {
+  //   printf("고등학생입니다.\n");
+  // }
+  // else
+  // {
+  //   printf("학생이 아닌가봐요\n");
+  // }
+  // return 0;
+
+  // break / continue
+  // 1번부터 30번까지 있는 반에서 1번에서 5번까지 조별 발표를 합니다.
+  // for(int i=1; i <= 30; i++)
+  // {
+  //   if(i>=6)
+  //   {
+  //     printf("나머지 학생은 집에 가세요.\n");
+  //     break;
+  //   }
+  //   printf("%d 번 학생은 조별 발표 준비를 하세요/\n", i);
+  // }
+
+  // 1번부터 30번까지 있는 반에서 7번 학생은 아파서 결석
+  // 7번을 제외하고 6번부터 10번까지 조별 발표를 하세요.
+  // for (int i=1; i<=30; i++)
+  // {
+  //   if (i>=6 && i<=10)
+  //   {
+  //     if(i==7)
+  //     {
+  //       printf("%d번 학생은 결석입니다.\n", i);
+  //       continue;
+  //     }
+  //     printf("%d 번 학생은 조별 발표 준비를 하세요.\n", i);
+  //   }
+  // }
+  // return 0;
+
+  // && ||
+//   int a = 10;
+//   int b = 10;
+//   int c = 12;
+//   int d = 12;
+//   if ( a==b || c==d)
+//   {
+//     printf("a 와 b, 혹은 c 와 d 의 값이 같습니다.\n");
+//   }
+//   else
+//   {
+//     printf("값이 서로 다르네요.\n");
+//   }
+
+  // 가위 0 바위 1 보 2
+  // srand(time(NULL));
+  // int i = rand() % 3; // 0 ~ 2 반환
+  // if ( i==0 )
+  // {
+  //   printf("가위\n");
+  // }
+  // else if ( i == 1 )
+  // {
+  //   printf("바위\n");
+  // }
+  // else if ( i == 2)
+  // {
+  //   printf("보\n");
+  // }
+  // else
+  // {
+  //   printf("몰라요\n");
+  // }
+
+  // return 0;
+
+  // srand(time(NULL));
+  // int i = rand() % 3; // 0 ~ 2 반환
+  // switch (i)
+  // {
+  //   case 0:printf("가위\n"); break;
+  //   case 1:printf("바위\n"); break;
+  //   case 2:printf("보\n"); break;
+  //   default:printf("몰라요\n"); break;
+  // }
+  // return 0;
+
+  int age = 11;
+  switch (age)
+  {
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+    case 13:printf("초등학생입니다\n"); break;
+    case 14:
+    case 15:
+    case 16:printf("중학생입니다\n"); break;
+    case 17:
+    case 18:
+    case 19:printf("고등학생입니다\n"); break;
+    default : printf("학생이 아닌가봐요\n"); break;
+  }
+ }
---
+++ function.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+
+// 선언
+void p(int num);
+void function_without_return();
+int function_with_return();
+void function_without_params();
+void function_with_params(int num1, int num2, int num3);
+
+int main_function(void)
+{
+  // // function
+  // // 계산기
+
+  // int num = 2;
+  // //printf("num 은 %d 입니다\n", num); // 2
+  // p(num);
+
+  // // 2 + 3 은?
+  // num = num + 3; // num += 3;
+  // //printf("num 은 %d 입니다\n", num); // 5
+  // p(num);
+
+  // // 5 - 1 은?
+  // num -= 1; // num = num - 1;
+  // //printf("num 은 %d 입니다\n", num); // 4
+  // p(num);
+
+  // // 4 x 3 은?
+  // num *= 3;
+  // //printf("num 은 %d 입니다\n", num); // 12
+  // p(num);
+
+  // // 12 / 6 은?
+  // num /= 6;
+  // //printf("num 은 %d입니다\n", num); // 2
+  // p(num);
+
+  // 함수 종류
+  // 반환값이 없는 함수
+  // function_without_return();
+
+  // 반환값이 있는 함수
+  // int ret = function_with_return();
+  // p(ret);
+
+  // 파라미터(전달값)가 없는 함수
+  // function_without_params();
+
+  // 파라미터(전달값)가 있는 함수
+  // function_with_params(1, 2, 3);
+
+  // 파라미터(전달값)도 받고, 반환값이 있는 함수
+  int ret = apple(5, 2); // 5개의 사과 중에서 2개를 ㅓㅁㄱ었어요
+  // printf("사과 5개 중에 2개를 먹으면? %d 개가 남아요\n", ret);
+  //printf("사과 5개 중에 2개를 먹으면? %d 개가 남아요\n", ret);
+  printf("사과 %d 개 중에 %d 개를 먹으면? %d가 남아요\n", 10, 4, apple(10, 4));
+ 
+ return 0;
+}
+
+// 정의
+void p(int num)
+{
+  printf("num 은 %d 입니다\n", num);
+}
+
+// 전달값 : 36
+// 함수 : ㅁ + 4
+// 반환형 : ?
+// int 함수이름(num)
+// {
+//   return num + 4;
+// }
+
+void function_without_return()
+{
+  printf("반환값이 없는 함수입니다.");
+}
+
+int function_with_return()
+{
+  printf("반환값이 있는 함수입니다\n");
+  return 10;
+}
+
+void function_without_params()
+{
+  printf("전달값이 없는 함수입니다.\n");
+}
+
+void function_with_params(int num1, int num2, int num3)
+{
+  printf("전달값이 있는 함수이며, 전달받은 값은 %d, %d, %d 입니다\n", num1, num2, num3);
+}
